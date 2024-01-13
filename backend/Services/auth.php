458159d1b5594853a0b57b6baad6f7d206cdb5fa
Saturnino Moreira Mendes <99210803+Saturnino1@users.noreply.github.com>
2024-01-13 22:12:00 -0100

    Update auth.php

--- backend/Services/auth.php
+++ backend/Services/auth.php
@@ -1,6 +1,10 @@
 <?php
-
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    
     include_once( __DIR__ . "/../connection.php");
+    include_once "token.php";
 
     class AuthService{
 
@@ -26,7 +30,17 @@
                 // return true;
                 $result = $run->fetchAll(PDO::FETCH_ASSOC);
                 $found = $run->rowCount();
-                $this->responseData($found,$result,200);
+                
+                if (isset( $result[0]  ) ) {
+                $userData = $result[0];
+                
+                    $token = tokenGenerator($userData['id'], $userData['email'], $userData['name'], $userData['photo_url'], 432000 );
+                    
+                    $this->responseData($found,$token,200);
+                }else{
+                    $this->responseData($found,null, 200);
+                }
+
                
             } catch (PDOException $e) {
                 echo json_encode([
@@ -47,12 +61,12 @@
 
         // }
 
-        private function responseData($found,$data, $status = 200){
+        private function responseData($found,$token, $status = 200){
             http_response_code($status);
             echo json_encode([
                 "status" => $status,
                 "total" => $found,
-                "data" => $data,
+                "token" => $token,
             ]);
         } 
         private function messageSend($msg, $status = 200){
